2a387a03579b4d72f2fbb3ad511e0255e9516545
thasami <thasami97@gmail.com>
2020-04-13 10:41:54 +0300

    second

--- consul2json.py
+++ consul2json.py
@@ -2,15 +2,48 @@ import consulate
 import toolz
 import json
 from collections import defaultdict
-
-session = consulate.Consul()    
+import argparse
+import sys
+import logging
+import traceback
 
 def nested_dict():
     return defaultdict(nested_dict)
 
-nd = nested_dict()
-for key, value in session.kv.iteritems():
-    nd.update(toolz.assoc_in(nd, key.split('/'), value)) 
+def checkIfKey(path,session):
+    check = toolz.get_in(path,session.kv,default='notDefined')
+    if check == 'notDefined':
+        return False
+    return True
 
-with open('test.json','w') as f:
-    json.dump(dict(nd),f)
+parser = argparse.ArgumentParser()
+parser.add_argument('-k','--key', dest='KEY',
+                        help="specifies key or path",required=True)
+parser.add_argument('-t','--token', dest='TOKEN',
+                    help="specify consul token",required=False)
+parser.add_argument('--host',dest='HOST', default='localhost', help="defines the host link of consul")
+parser.add_argument('-p','--port',dest='PORT', default='8500', help="defines the host port of consul")
+parser.add_argument('-s','--scheme',dest='SCHEME', default='http', help="defines connection scheme")
+
+args = parser.parse_args()
+
+session = consulate.Consul(host=args.HOST, port=args.PORT,
+                           scheme=args.SCHEME, token=args.TOKEN)    
+
+
+
+if checkIfKey(args.KEY,session):
+    with open('test.json','w') as f:
+        json.dump(session.kv[args.KEY],f)
+    sys.exit(0)
+
+try:
+    nd = nested_dict()
+    for key, value in session.kv.find(args.KEY).items():
+        nd.update(toolz.assoc_in(nd, key.split('/'), value)) 
+
+    with open('test.json','w') as f:
+        json.dump(dict(nd),f)
+        
+except Exception as e:
+        logging.error(traceback.format_exc())
