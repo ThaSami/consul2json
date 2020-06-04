b2c7f13fe7ebcede9a2e90a90fa2e4748d69aa11
Sami Shabaneh <25616506+ThaSami@users.noreply.github.com>
2020-06-04 14:36:08 +0300

    dump output to a file
    Feature; adding -f <file> as argument to dump output to a file

--- consul2json.py
+++ consul2json.py
@@ -8,15 +8,15 @@ import traceback
 import json
 
 class Consul2Json():
-    
+
 
     def __init__(self,host,port,scheme,token):
         self.host = host
-        self.port = port 
-        self.scheme = scheme 
+        self.port = port
+        self.scheme = scheme
         self.token = token
         self.session = consulate.Consul(host=host, port=port,
-                           scheme=scheme, token=token)    
+                                        scheme=scheme, token=token)
 
     def nested_dict(self):
         return defaultdict(self.nested_dict)
@@ -37,10 +37,10 @@ class Consul2Json():
         except Exception as x:
             logging.error(traceback.format_exc())
             return json.dumps(traceback.format_exc())
-    
+
 
     def getIfPath(self,path):
-        ''' returns a structured nested dictionary with all values and keys in a given path ''' 
+        ''' returns a structured nested dictionary with all values and keys in a given path '''
         if not path.endswith('/'):
             path+='/'
         try:
@@ -48,12 +48,12 @@ class Consul2Json():
             keylen = len(path.split('/'))
             if path.endswith('/'):
                 keylen = len(path.split('/')) - 1
-                
+
             for key, value in self.session.kv.find(path).items():
-                keysToPut = [i for i in key.split('/')[keylen:]] 
+                keysToPut = [i for i in key.split('/')[keylen:]]
                 nd.update(toolz.assoc_in(nd, keysToPut, value))
             if len(nd) > 0:
-                return json.dumps(dict(nd))  
+                return json.dumps(dict(nd))
             return 'Not Defined'
 
         except Exception as e:
@@ -66,9 +66,9 @@ class Consul2Json():
             return self.getKey(path)
         return self.getIfPath(path)
 
-    def heartBeat(self):
+    def heartBeat(self,key):
         try:
-            check = self.session.kv['ping']
+            check = self.session.kv[key]
             return True
         except:
             return False
@@ -76,18 +76,20 @@ class Consul2Json():
 
     def describe(self):
         return "host {},port {}, scheme {}, token {}".format(self.host,self.port,self.scheme,self.token)
-    
+
 
 def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('-k','--key', dest='KEY',
-                            help="specifies key or path",required=True)  
+                        help="specifies key or path",required=True)
 
     parser.add_argument('-t','--token', dest='TOKEN',
                         help="specify consul token",required=False)
     parser.add_argument('--host',dest='HOST', default='localhost', help="defines the host link of consul")
     parser.add_argument('-p','--port',dest='PORT', default='8500', help="defines the host port of consul")
     parser.add_argument('-s','--scheme',dest='SCHEME', default='http', help="defines connection scheme")
+    parser.add_argument('-f','--file', dest='FILE',
+                            help="specifies the file to put data in",required=True)     
 
     args = parser.parse_args()
     return args
@@ -96,5 +98,8 @@ def getArgs():
 if __name__ == "__main__":
     args = getArgs()
     c2j = Consul2Json(host=args.HOST, port=args.PORT,
-                           scheme=args.SCHEME, token=args.TOKEN)    
-    print(c2j.getVal(args.KEY))
+                      scheme=args.SCHEME, token=args.TOKEN)
+
+    with open(args.FILE,'w') as f:
+        f.write(c2j.getVal(args.KEY))              
+    
